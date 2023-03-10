d326a24d5eeecd8695e5c60b7329a3484a0a82c7
Batı Gökovalı <batigokovali@icloud.com>
2023-03-10 18:44:39 +0300

    Update

--- src/components/Backoffice.jsx
+++ src/components/Backoffice.jsx
@@ -3,6 +3,8 @@ import { Container } from "react-bootstrap";
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Button from 'react-bootstrap/Button';
+import { useState, useEffect, useRef } from "react";
+import { ImAttachment } from 'react-icons/im'
 
 
 const Backoffice = () => {
@@ -11,8 +13,70 @@ const Backoffice = () => {
     const imdbID = document.querySelector("#movie-input-imdbID")
     const type = document.querySelector("#movie-input-type")
 
-    const postMedia = async () => {
+    const [file, setFile] = useState();
+    const [isError, setError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("")
+    const inputRef = useRef(null);
 
+    function handleFile(event) {
+        const img = event.target.files[0];
+        const imgData = new FormData();
+        imgData.append("poster", img);
+        setFile(imgData)
+        console.log(event.target.files[0]);
+    }
+
+    const handleClick = () => {
+        inputRef.current.click();
+    };
+
+    const postMedia = async () => {
+        try {
+            if (file) {
+                const newMedia = {
+                    title: title.value,
+                    year: year.value,
+                    imdbID: imdbID.value,
+                    type: type.value,
+                    poster: "",
+                };
+                const res = await fetch(process.env.REACT_APP_BE_DEV_URL + "/medias", {
+                    method: "POST",
+                    body: JSON.stringify(newMedia),
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                });
+                if (res.ok) {
+                    const resMessage = await res.json()
+                    const imdbID = resMessage.imdbID;
+                    console.log(imdbID)
+                    const res2 = await fetch(process.env.REACT_APP_BE_DEV_URL + "/medias/" + imdbID + "/poster", {
+                        method: "POST",
+                        body: file,
+                    });
+                    if (res2.ok) {
+                        setFile(null);
+                        title.value = "";
+                        year.value = "";
+                        type.value = "";
+                    } else {
+                        setErrorMessage("Error about your poster!")
+                        setError(true)
+                    }
+                }
+                else {
+                    setErrorMessage("Error about your media content!")
+                    setError(true)
+                }
+            } else {
+                setErrorMessage("You should pick a poster for a new media!")
+                setError(true)
+            }
+        } catch (error) {
+            setErrorMessage("ERROR :D")
+            setError(true)
+        }
     }
 
     return (
@@ -64,11 +128,25 @@ const Backoffice = () => {
             <Row className="justify-content-center">
                 <Col lg={6}>
                     <Form.Group controlId="formFile" className="mb-3">
-                        <Form.Control type="file" />
+                        <Form.Control style={{ display: "none" }} onChange={handleFile} type="file" name="file" ref={inputRef} />
                     </Form.Group>
                 </Col>
             </Row>
-            <Button variant="primary">Submit Media</Button>
+            <Row className="justify-content-center">
+                <Col sm={2} md={2} lg={2}>
+                    <Button variant="primary" onClick={handleClick}><ImAttachment></ImAttachment>Add Photo</Button>
+                </Col>
+                <Col sm={2} md={2} lg={2}>
+                    <Button variant="primary" onClick={postMedia}>Submit</Button>
+                </Col>
+            </Row>
+            <Row className="justify-content-center">
+                <Col sm={2} md={2} lg={6}>
+
+                </Col>
+            </Row>
+
+
         </Container>
     )
 }
